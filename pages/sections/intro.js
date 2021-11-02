65c83c4ae17b6acccbfbb09e0ffa99c8d9a60749
Pedro Costa <pedro.j.costa@tecnico.ulisboa.pt>
2021-11-02 10:51:14 +0000

    Removed superfluous paragraph

--- pages/sections/intro.js
+++ pages/sections/intro.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from "react";
 import {
   Menu,
   Sidebar,
@@ -13,8 +13,8 @@ import {
   Divider,
   List,
   Image,
-} from 'semantic-ui-react';
-import { Link } from '../../routes';
+} from "semantic-ui-react";
+import { Link } from "../../routes";
 
 class LayoutMenu extends Component {
   state = {
@@ -33,39 +33,39 @@ class LayoutMenu extends Component {
         stackable
         verticalAlign="top"
         style={{
-          padding: '2em 0em',
+          padding: "2em 0em",
           /*backgroundImage: 'url(/backgnd.jpg)',*/
-          backgroundColor: 'rgba(28, 173, 229, 1.0)',
+          backgroundColor: "rgba(28, 173, 229, 1.0)",
           /* Create the parallax scrolling effect */
-          backgroundAttachment: 'fixed',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no - repeat',
-          backgroundSize: 'cover',
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center",
+          backgroundRepeat: "no - repeat",
+          backgroundSize: "cover",
         }}
       >
         <Grid.Row
           fluid
           style={{
-            backgroundColor: 'rgba(212, 32, 32, 0.0)',
-            marginTop: '80px',
-            padding: '0em 3em',
-            marginBottom: '40px',
+            backgroundColor: "rgba(212, 32, 32, 0.0)",
+            marginTop: "80px",
+            padding: "0em 3em",
+            marginBottom: "40px",
           }}
         >
           <Grid.Column
             floated="left"
             width={2}
-            style={{ paddingLeft: '4em', textAlign: 'left' }}
+            style={{ paddingLeft: "4em", textAlign: "left" }}
           ></Grid.Column>
           <Grid.Column
             floated="left"
             width={12}
-            style={{ paddingLeft: '4em', textAlign: 'center' }}
+            style={{ paddingLeft: "4em", textAlign: "center" }}
           >
-            <Header as="h1" style={{ color: 'rgba(240,240,240,1.0)' }}>
+            <Header as="h1" style={{ color: "rgba(240,240,240,1.0)" }}>
               <b>An extended portfolio to meet your needs.</b>
               <Header.Subheader
-                style={{ color: 'rgba(210,210,210,1.0)', padding: '0em 1em' }}
+                style={{ color: "rgba(0,33,89, 1.0)", padding: "0em 1em" }}
               >
                 <br />
                 PHI Consulting is an agency that supports companies and
@@ -77,126 +77,33 @@ class LayoutMenu extends Component {
           <Grid.Column
             floated="left"
             width={2}
-            style={{ paddingLeft: '4em', textAlign: 'left' }}
-          ></Grid.Column>
-        </Grid.Row>
-        <Grid.Row
-          fluid
-          style={{
-            backgroundColor: 'rgba(212, 32, 32, 0.0)',
-            padding: '0em 3em',
-            marginBottom: '2em',
-          }}
-        >
-          <Grid.Column
-            floated="left"
-            width={2}
-            style={{
-              paddingLeft: '4em',
-
-              textAlign: 'left',
-            }}
-          ></Grid.Column>
-          <Grid.Column
-            floated="left"
-            width={6}
-            style={{
-              paddingLeft: '4em',
-              textAlign: 'left',
-            }}
-          >
-            <List>
-              <List.Item style={{ marginBottom: '10px' }}>
-                <List.Icon
-                  name="check circle"
-                  size="large"
-                  style={{ color: 'rgba(240, 240, 240, 1.0)' }}
-                />
-                <List.Content
-                  style={{
-                    fontSize: '1.3em',
-                    color: 'rgba(240, 240, 240, 1.0)',
-                  }}
-                >
-                  PHI's portfolio includes access to the <b>financing models</b>{' '}
-                  of companies and people in the alternative finance markets
-                  (AltFi), crowdsourcing platforms, peer-to-peer financing
-                  (private-to-private), initial crypto-currency offering (ICO),
-                  and various modalities of Decentralized Finance.
-                  <br />
-                  <a
-                    style={{ color: 'rgba(0,33,89, 1.0)' }}
-                    href="/#ServiceGrid"
-                  >
-                    Learn More
-                    <Icon
-                      name="angle right"
-                      style={{ color: 'rgba(0,33,89, 1.0)' }}
-                    />
-                  </a>
-                </List.Content>
-              </List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column
-            floated="left"
-            width={6}
-            style={{
-              paddingLeft: '4em',
-              textAlign: 'left',
-            }}
-          >
-            <List>
-              <List.Item style={{ marginBottom: '10px' }}>
-                <List.Icon
-                  name="check circle"
-                  size="large"
-                  style={{ color: 'rgba(240, 240, 240,1.0)' }}
-                />
-                <List.Content
-                  style={{
-                    fontSize: '1.3em',
-                    color: 'rgba(240, 240, 240, 1.0)',
-                  }}
-                >
-                  The use of crypto-currency payments is developing: we provide
-                  the means to create your own <b>e-commerce</b> in
-                  crypto-currency.
-                  <br />
-                  <a style={{ color: 'rgba(0,33,89, 1.0)' }} href="/#Platforms">
-                    Learn More
-                    <Icon
-                      name="angle right"
-                      style={{ color: 'rgba(0,33,89, 1.0)' }}
-                    />
-                  </a>
-                </List.Content>
-              </List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column
-            floated="left"
-            width={2}
-            style={{ paddingLeft: '4em', textAlign: 'left' }}
-          ></Grid.Column>
-        </Grid.Row>
-        <Grid.Row
-          fluid
-          style={{
-            backgroundColor: 'rgba(212, 32, 32, 0.0)',
-            padding: '0em 3em',
-            marginBottom: '70px',
-          }}
-        >
-          <Grid.Column
-            floated="left"
-            width={2}
-            style={{ paddingLeft: '4em', textAlign: "left" }}
-          ></Grid.Column>
-          <Grid.Column
-            floated="left"
-            width={6}
             style={{ paddingLeft: "4em", textAlign: "left" }}
+          ></Grid.Column>
+        </Grid.Row>
+        <Grid.Row
+          fluid
+          style={{
+            backgroundColor: "rgba(212, 32, 32, 0.0)",
+            padding: "0em 3em",
+            marginBottom: "2em",
+          }}
+        >
+          <Grid.Column
+            floated="left"
+            width={2}
+            style={{
+              paddingLeft: "4em",
+
+              textAlign: "left",
+            }}
+          ></Grid.Column>
+          <Grid.Column
+            floated="left"
+            width={4}
+            style={{
+              paddingLeft: "0em",
+              textAlign: "left",
+            }}
           >
             <List>
               <List.Item style={{ marginBottom: "10px" }}>
@@ -211,11 +118,16 @@ class LayoutMenu extends Component {
                     color: "rgba(240, 240, 240, 1.0)",
                   }}
                 >
-                  The exponential growth of Uber-type on-demand services fuels
-                  the <b>platform</b> industry. Bring your business into the
-                  on-demand platforms and exponentiate your growth.
+                  PHI's portfolio includes access to the <b>financing models</b>{" "}
+                  of companies and people in the alternative finance markets
+                  (AltFi), crowdsourcing platforms, peer-to-peer financing
+                  (private-to-private), initial crypto-currency offering (ICO),
+                  and various modalities of Decentralized Finance.
                   <br />
-                  <a style={{ color: "rgba(0,33,89, 1.0)" }} href="/#Platforms">
+                  <a
+                    style={{ color: "rgba(0,33,89, 1.0)" }}
+                    href="/#ServiceGrid"
+                  >
                     Learn More
                     <Icon
                       name="angle right"
@@ -228,7 +140,7 @@ class LayoutMenu extends Component {
           </Grid.Column>
           <Grid.Column
             floated="left"
-            width={6}
+            width={4}
             style={{ paddingLeft: "4em", textAlign: "left" }}
           >
             <List>
@@ -266,9 +178,67 @@ class LayoutMenu extends Component {
           </Grid.Column>
           <Grid.Column
             floated="left"
+            width={4}
+            style={{ paddingLeft: "4em", textAlign: "left" }}
+          >
+            <List>
+              <List.Item style={{ marginBottom: "10px" }}>
+                <List.Icon
+                  name="check circle"
+                  size="large"
+                  style={{ color: "rgba(240, 240, 240, 1.0)" }}
+                />
+                <List.Content
+                  style={{
+                    fontSize: "1.3em",
+                    color: "rgba(240, 240, 240, 1.0)",
+                  }}
+                >
+                  The exponential growth of Uber-type on-demand services fuels
+                  the <b>platform</b> industry. Bring your business into the
+                  on-demand platforms and exponentiate your growth.
+                  <br />
+                  <a style={{ color: "rgba(0,33,89, 1.0)" }} href="/#Platforms">
+                    Learn More
+                    <Icon
+                      name="angle right"
+                      style={{ color: "rgba(0,33,89, 1.0)" }}
+                    />
+                  </a>
+                </List.Content>
+              </List.Item>
+            </List>
+          </Grid.Column>
+
+          <Grid.Column
+            floated="left"
             width={2}
             style={{ paddingLeft: "4em", textAlign: "left" }}
+          />
+        </Grid.Row>
+        <Grid.Row
+          fluid
+          style={{
+            backgroundColor: "rgba(212, 32, 32, 0.0)",
+            padding: "0em 3em",
+            marginBottom: "70px",
+          }}
+        >
+          <Grid.Column
+            floated="left"
+            width={4}
+            style={{ paddingLeft: "4em", textAlign: "left" }}
           ></Grid.Column>
+          <Grid.Column
+            floated="left"
+            width={8}
+            style={{ paddingLeft: "4em", textAlign: "left" }}
+          ></Grid.Column>
+          <Grid.Column
+            floated="left"
+            width={4}
+            style={{ paddingLeft: "4em", textAlign: "left" }}
+          />
         </Grid.Row>
       </Grid>
     );
